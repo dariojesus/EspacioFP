0d1d59cd7e0647a241daecb214320700feac473d
dariojesus <37369686+dariojesus@users.noreply.github.com>
2020-03-01 14:51:15 +0100

    Ejercicio semana blanca

---
+++ prPracticasComplejas/src/prPractica14/Piloto.java
@@ -0,0 +1,47 @@
+package prPractica14;
+
+public class Piloto {
+	
+	private String nombre;
+	private String nacionalidad;
+	private String fechaNac;
+	private String escuderia;
+	private int puntuacion;
+	
+	public Piloto(String nombre, String nacionalidad, String fechaNac, String escuderia, int puntuacion) {
+		super();
+		this.nombre = nombre;
+		this.nacionalidad = nacionalidad;
+		this.fechaNac = fechaNac;
+		this.escuderia = escuderia;
+		this.puntuacion = puntuacion;
+	}
+	
+	
+	public String getNombre() {return nombre;}
+	
+	public void setNombre(String nombre) {this.nombre = nombre;}
+	
+	public String getNacionalidad() {return nacionalidad;}
+	
+	public void setNacionalidad(String nacionalidad) {this.nacionalidad = nacionalidad;}
+	
+	public String getFechaNac() {return fechaNac;}
+	
+	public void setFechaNac(String fechaNac) {this.fechaNac = fechaNac;}
+	
+	public String getEscuderia() {return escuderia;}
+	
+	public void setEscuderia(String escuderia) {this.escuderia = escuderia;}
+	
+	public int getPuntuacion() {return puntuacion;}
+	
+	public void setPuntuacion(int puntuacion) {this.puntuacion = puntuacion;}
+	
+	public String toString () {
+		return this.getPuntuacion()+" - "+this.getNombre()+" ("+this.getNacionalidad()+"), "+this.getEscuderia()+" - "+this.getFechaNac();
+	}
+	
+	
+
+}
---
+++ prPracticasComplejas/src/prPractica14/PilotosF1.java
@@ -0,0 +1,101 @@
+package prPractica14;
+
+import java.util.Scanner;
+
+public class PilotosF1 {
+	
+	private Piloto [] pilotos;
+	private int posLibre = 0;
+	
+	public PilotosF1() {
+		pilotos = new Piloto [22];
+	}
+
+	public Piloto[] getPilotos() {return pilotos;}
+
+	public void setPilotos(Piloto[] pilotos) {this.pilotos = pilotos;}
+
+	public int getPosLibre() {return posLibre;}
+
+	public void setPosLibre(int posLibre) {this.posLibre = posLibre;}
+	
+	public boolean addPiloto(Piloto p) {
+		int pos = this.getPosLibre();
+		
+		if ( pos < 22) {
+			pilotos[pos]=p;
+			this.setPosLibre(pos+1);
+			return true;
+		}
+		
+		else return false;
+	}
+	
+	public int obtenerPuntos(int puesto) {
+		switch (puesto) {
+			case 1: return 25;
+			case 2: return 18;
+			case 3: return 15;
+			case 4: return 12;
+			case 5: return 10;
+			case 6: return 8;
+			case 7: return 6;
+			case 8: return 4;
+			case 9: return 2;
+			case 10: return 1;
+			default: return 0;
+		}
+	}
+	
+	public boolean addCarreraPiloto(String nombre, int puesto) {
+		Piloto p = buscaPiloto(nombre);
+		
+		if (p != null) {
+			p.setPuntuacion(p.getPuntuacion()+this.obtenerPuntos(puesto));
+			return true;
+		}
+		
+		else return false;
+		
+		
+	}
+	
+	private Piloto buscaPiloto(String nombre) {
+		boolean encontrado = false;
+		int cont = 0;
+		
+		while (!encontrado && cont < this.getPosLibre()) {
+			if (pilotos[cont].getNombre().equalsIgnoreCase(nombre))encontrado=true;
+			else cont++;
+		}
+		
+		return encontrado?pilotos[cont]:null;
+	}
+	
+	public void addGranPremio(String ganadores) {
+		int puesto = 1;
+		Scanner sc = new Scanner (ganadores);
+		sc.useDelimiter(";");
+		
+		
+		while (sc.hasNext()) {
+			this.addCarreraPiloto(sc.nextLine(),puesto);
+			puesto++;
+		}
+		
+		sc.close();
+	}
+	
+	public String clasificacionPorPilotos() {
+		//Ordenar los pilotos primero
+		//Mostrar los pilotos depues
+	}
+	
+	private void ordenaPuntos() {
+		for(int i = 0 ; i < this.getPosLibre() ; i++) {
+			for (int j = i+1 ; j < this.getPosLibre()-1 ; j++) {
+				if ()
+			}
+		}
+	}
+}
